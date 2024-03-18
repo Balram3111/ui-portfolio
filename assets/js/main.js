57524b6784fda90a0293aa8c20982cc6af971066
Balram3111 <balu59445@gmail.com>
2024-03-18 15:36:39 +0530

    updated2.0
    updated projects links about us and working contact form

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,7 @@
+
+
+
+
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 function scrollHeader() {
   const header = document.getElementById("header");
@@ -58,6 +62,7 @@ workLinks.forEach((wl) => {
     activeWork(wl);
   });
 });
+
 
 /*=============== SWIPER TESTIMONIAL ===============*/
 
@@ -230,4 +235,43 @@ sr.reveal(`.footer, footer__container`, {
   scale: 0.9,
   origin: "bottom",
   distance: "30px",
+});
+
+const form = document.getElementById('form');
+const result = document.getElementById('result');
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const object = Object.fromEntries(formData);
+  const json = JSON.stringify(object);
+  result.innerHTML = "Please wait..."
+
+    fetch('https://api.web3forms.com/submit', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: json
+        })
+        .then(async (response) => {
+            let json = await response.json();
+            if (response.status == 200) {
+                result.innerHTML = "Form submitted successfully";
+            } else {
+                console.log(response);
+                result.innerHTML = json.message;
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            result.innerHTML = "Something went wrong!";
+        })
+        .then(function() {
+            form.reset();
+            setTimeout(() => {
+                result.style.display = "none";
+            }, 3000);
+        });
 });
